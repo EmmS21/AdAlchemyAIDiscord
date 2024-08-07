c68ccfccc8d7efe29c68147e58d86cfb07e9bf26
EmmS21 <44911198+EmmS21@users.noreply.github.com>
2024-08-06 18:48:02 -0700

    Edited workflow when user rejects consent form

--- Helpers/helperClasses.py
+++ Helpers/helperClasses.py
@@ -3,6 +3,7 @@ from discord import app_commands, ButtonStyle, Embed, File
 from discord.ui import Button, View, TextInput, Modal
 from collections import defaultdict
 from MongoDBConnection.connectMongo import connect_to_mongo_and_get_collection
+import os
 
 guild_business_data = defaultdict(dict)
 guild_states = {}
@@ -11,9 +12,23 @@ class ConfirmPricing(discord.ui.View):
     def __init__(self, guild_id):
         super().__init__()
         self.guild_id = guild_id
+        self.is_second_chance = False
 
     @discord.ui.button(label="Yes", style=discord.ButtonStyle.green)
     async def yes(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await self.handle_yes_response(interaction)
+
+    @discord.ui.button(label="No", style=discord.ButtonStyle.red)
+    async def no(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if not self.is_second_chance:
+            self.is_second_chance = True
+            await interaction.response.send_message("Are you sure? You can still join our waiting list or exit the conversation.", view=self)
+        else:
+            await interaction.response.send_message("If you would like to restart the process, add the bot to a new server. Alternatively, feel free to email emmanuel@emmanuelsibanda.com if you have any questions.")
+            guild_states[self.guild_id] = "conversation_ended"
+            self.stop()
+            
+    async def handle_yes_response(self, interaction: discord.Interaction):
         guild = interaction.guild
         owner = guild.owner
 
@@ -58,11 +73,5 @@ class ConfirmPricing(discord.ui.View):
 
         await interaction.channel.send(embed=embed)
 
-        guild_states[self.guild_id] = "setup_complete"
-        self.stop()
-
-    @discord.ui.button(label="No", style=discord.ButtonStyle.red)
-    async def no(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_message("You have not consented to the pricing model. If you have any questions or concerns, please reach out to our support team.")
         guild_states[self.guild_id] = "setup_complete"
         self.stop()
